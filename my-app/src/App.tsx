e414a6aa58c2d3982e3669573153d7fd3c1b3c41
somurie06 <somurie06@gmail.com>
2020-05-11 22:30:46 +0900

    タイトルを追加

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,11 +3,15 @@ import './App.css';
 import {Songs} from "./features/song/Songs";
 import {SongArchive} from "./features/songArchive/SongArchive";
 import {AudioPlayerComponent} from "./features/audio/AudioPlayerComponent";
+import {Typography} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <div>
+        <Typography variant='h3'>
+          百人一首読み上げ
+        </Typography>
         <AudioPlayerComponent />
         <SongArchive />
         <Songs />
