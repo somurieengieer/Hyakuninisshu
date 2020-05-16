282ac99da8b1b1589c95af1cd80709eaf0022917
somurie06 <somurie06@gmail.com>
2020-05-16 11:34:28 +0900

    モーダルのレイアウトを修正。高さを最大化して右下に画像も表示する

--- my-app/src/features/player/ModalPlayer.tsx
+++ my-app/src/features/player/ModalPlayer.tsx
@@ -6,51 +6,57 @@ import {theme} from "../../materialui/theme";
 
 
 const useStyles = makeStyles({
+  modal: {
+    margin: theme.spacing(2),
+    height: 'calc(100vh * 0.9)',
+  },
+
+  // ModalPlayerの背景画像設定
   paper: {
     backgroundImage: `url(${process.env.PUBLIC_URL}/image/washi.jpg)`,
   },
   paperTop: {
     backgroundImage: `url(${process.env.PUBLIC_URL}/image/wagara_top.png)`,
     backgroundRepeat: 'no-repeat',
+    minHeight: '150px' //背景画像の枠を確保するため
   },
   paperBottom: {
     backgroundImage: `url(${process.env.PUBLIC_URL}/image/wagara_bottom.png)`,
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'bottom right 0px',
+    // bottom: 0,
+    minHeight: '150px' //背景画像の枠を確保するため
   },
-  iconBig: {
-    [theme.breakpoints.down('xs')]: {
-      fontSize: '6em'
-    },
-    [theme.breakpoints.up('sm')]: {
-      fontSize: '10em'
-    }
+
+  // フッターを固定化するためのレイアウト
+  layout_body: {
+    display: 'flex',
+    flexFlow: 'column',
+    height: '100%'
   },
-  iconSmall: {
-    [theme.breakpoints.down('xs')]: {
-      fontSize: '3em'
-    },
-    [theme.breakpoints.up('sm')]: {
-      fontSize: '6em'
-    }
+  layout_header: {
+    // height: '40px', // 背景画像などに合わせて設定
   },
-  songArea: {
-    [theme.breakpoints.down('xs')]: {
-      width: 400
-    },
-    [theme.breakpoints.up('sm')]: {
-      width: 560,
-    }
+  layout_main: {
+    flex: 1,
+
+    // モーダルのサイズが変更しないようmain内のサイズが大きくなりすぎない制御
+    maxHeight: 'calc(100vh - 150px*2)',
+    overflow: 'scroll',
+  },
+  layout_footer: {
+    // height: '40px', // 背景画像などに合わせて設定
   },
 });
 
 interface ModalPlayerProps {
   callbackStop: () => void
+  footerJSX?: JSX.Element
 }
 
 // 序歌も含めて流す連番を引数とする
 export const ModalPlayer: React.FC<ModalPlayerProps> =
-  ({callbackStop, children}) => {
+  ({callbackStop, footerJSX, children}) => {
 
     const classes = useStyles();
 
@@ -64,8 +70,9 @@ export const ModalPlayer: React.FC<ModalPlayerProps> =
         onClose={stop}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
+        className={[classes.modal,].join(' ')}
       >
-        <Box m={1} p={1} className={classes.paper}>
+        <Box m={1} p={1} className={[classes.paper, classes.layout_body].join(' ')}>
           <Box className={classes.paperTop}>
             <Box display="flex" justifyContent="flex-end" p={1}>
               <IconButton onClick={stop}
@@ -75,8 +82,15 @@ export const ModalPlayer: React.FC<ModalPlayerProps> =
             </Box>
           </Box>
 
-          {children}
+          <Box className={classes.layout_main}>
+            {children}
+          </Box>
 
+          <Box display="flex" justifyContent="center"
+               className={[classes.paperBottom, classes.layout_footer].join(' ')}
+          >
+            {footerJSX}
+          </Box>
         </Box>
       </Modal>
     );
