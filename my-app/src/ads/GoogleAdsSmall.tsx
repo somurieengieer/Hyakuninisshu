8955ef8d203289a7727ef89204447ce971dfbe0a
somurie06 <somurie06@gmail.com>
2020-05-19 09:04:53 +0900

    広告が表示されるよう修正（バグ修正）

--- my-app/src/ads/GoogleAdsSmall.tsx
+++ my-app/src/ads/GoogleAdsSmall.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 
 export function GoogleAdsSmall() {
 
+  useEffect(() => {
+    // @ts-ignore
+    if (window.adsbygoogle && process.env.NODE_ENV === "production") {
+      // @ts-ignore
+      window.adsbygoogle.push({});
+    }
+  }, [])
+
   return (
     <>
       {/*Google adsense advertisement */}
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <ins className="adsbygoogle"
            style={{display: 'block', textAlign: 'center'}}
            data-ad-layout="in-article"
            data-ad-format="fluid"
            data-ad-client="ca-pub-7646050836920456"
            data-ad-slot="9249145427"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
     </>
   )
 }
