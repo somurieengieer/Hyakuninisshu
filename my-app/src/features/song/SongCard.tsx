10048a09886d9c012b495049f2e8003f42a07ffa
somurie06 <somurie06@gmail.com>
2020-06-08 18:01:56 +0900

    下の句カード上で文字が15文字を超過するとカードから文字が溢れるバグを修正

--- my-app/src/features/song/SongCard.tsx
+++ my-app/src/features/song/SongCard.tsx
@@ -19,6 +19,11 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     fontSize: '2.7rem', // カードのサイズを固定にするため直書き。variant='h3'の書き方はNG
     lineHeight: '3.6rem',
+  },
+  textOverSize: {
+    width: '33%',
+    textAlignLast: 'justify',
+    letterSpacing: '-0.2em',
   }
 }));
 
@@ -31,11 +36,27 @@ export function SongCard({songInfo}: SongProps) {
 
   const classes = useStyles();
 
+  const isOverSize = songInfo.song_hiragana_shimo().length > 15
+
   return (
     <Box className={classes.root}>
-      <Typography className={classes.text}>
-        {songInfo.song_hiragana_shimo()}
-      </Typography>
+      {isOverSize ? (
+        <>
+          <Typography className={[classes.text, classes.textOverSize].join(' ')}>
+            {songInfo.song_hiragana_shimo().slice(0, 5)}
+          </Typography>
+          <Typography className={[classes.text, classes.textOverSize].join(' ')}>
+            {songInfo.song_hiragana_shimo().slice(5, 11)}
+          </Typography>
+          <Typography className={[classes.text, classes.textOverSize].join(' ')}>
+            {songInfo.song_hiragana_shimo().slice(11)}
+          </Typography>
+        </>
+      ) : (
+        <Typography className={classes.text}>
+          {songInfo.song_hiragana_shimo()}
+        </Typography>
+      )}
     </Box>
   );
 }
